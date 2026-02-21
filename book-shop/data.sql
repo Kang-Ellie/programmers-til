392dd5726cce35b0dec262a3beda75f1d8918371
Ellie-kang <kya6873@hanmail.net>
2026-02-21 18:47:01 +0900

    ⚙️ chore: 도서 조회 및 좋아요 기능 테스트 데이터 추가

--- book-shop/data.sql
+++ book-shop/data.sql
@@ -11,4 +11,31 @@ INSERT INTO books (title, form, isbn, summary, detail, author, pages, contents, 
 VALUES ("흥부와 놀부들", "종이책", 3, "제비..", "까만 제비..", "김제비", 100, "목차입니다.", 20000, "2026-02-08");
 
 SELECT * FROM Bookshop.books LEFT
-JOIN category ON books.category_id = category_id WHERE books.id=1;
+JOIN category ON books.category_id = category_id;
+
+SELECT * FROM Bookshop.books LEFT JOIN category ON books.category_id = category_id WHERE books.id=1;
+
+INSERT INTO likes (user_id, liked_book_id) VALUES (1, 1);
+INSERT INTO likes (user_id, liked_book_id) VALUES (1, 2);
+INSERT INTO likes (user_id, liked_book_id) VALUES (1, 3);
+INSERT INTO likes (user_id, liked_book_id) VALUES (4, 4);
+INSERT INTO likes (user_id, liked_book_id) VALUES (2, 1);
+INSERT INTO likes (user_id, liked_book_id) VALUES (2, 2);
+INSERT INTO likes (user_id, liked_book_id) VALUES (2, 3);
+INSERT INTO likes (user_id, liked_book_id) VALUES (2, 6);
+INSERT INTO likes (user_id, liked_book_id) VALUES (4, 1);
+
+
+DELETE FROM likes WHERE user_id = 1 AND liked_book_id = 3;
+
+// 테이블의 조건을 만족하는 행 갯수
+SELECT count(*) FROM Bookshop.likes WHERE liked_book_id=1
+
+// 좋아요 개수가 포함된 books 테이블 조회
+SELECT *, (SELECT count(*) FROM likes WHERE books.id=liked_book_id) AS likes FROM books
+
+// 개별 도서 조회 시, 사용자가 좋아요를 했는지 여부를 포함
+SELECT * FROM books WHERE books.id=1;
+
+SELECT EXISTS (SELECT * FROM likes WHERE user_id=1 AND liked_book_id=1);
+= SELECT count(*) FROM likes WHERE user_id=1 AND liked_book_id=1
