0a1225d5e008a9493cfe9c456a1b4581256d329e
Ellie-kang <kya6873@hanmail.net>
2026-02-21 12:43:23 +0900

    ⚙️ chore: 더미 데이터 SQL 데이터 추가

--- book-shop/data.sql
+++ book-shop/data.sql
@@ -9,3 +9,6 @@ VALUES ("백설공주들", "종이책", 2, "사과..", "빨간 사과..", "김�
 
 INSERT INTO books (title, form, isbn, summary, detail, author, pages, contents, price, pub_date)
 VALUES ("흥부와 놀부들", "종이책", 3, "제비..", "까만 제비..", "김제비", 100, "목차입니다.", 20000, "2026-02-08");
+
+SELECT * FROM Bookshop.books LEFT
+JOIN category ON books.category_id = category_id WHERE books.id=1;
