c5ce4c1944da5c618b054f004bd76e062d1ca23f
Ellie-kang <kya6873@hanmail.net>
2026-03-22 20:47:34 +0900

    ✨ feat: Task 및 ActionButton 컴포넌트 구현 (UI 및 공통 로직)

--- task-app/src/components/List/List.tsx
+++ task-app/src/components/List/List.tsx
@@ -65,7 +65,7 @@ const List: FC<TListProps> = ({ list, boardId }) => {
           />
         </div>
       ))}
-      <ActionButton />
+      <ActionButton boardId={boardId} listId={list.listId} />
     </div>
   );
 };
