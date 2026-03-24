bebd8f67d041f05098db83be35dc48105cee72c6
Ellie-kang <kya6873@hanmail.net>
2026-03-24 09:59:46 +0900

    ✨ feat: EditModal 스타일링 및 레이아웃 구현

--- task-app/src/components/EditModal/EditModal.tsx
+++ task-app/src/components/EditModal/EditModal.tsx
@@ -8,6 +8,17 @@ import {
 } from "../../store/slices/boardsSlice";
 import { addLog } from "../../store/slices/loggerSlice";
 import { v4 } from "uuid";
+import {
+  buttons,
+  closeButton,
+  deleteButton,
+  header,
+  input,
+  modalWindow,
+  title,
+  updateButton,
+  wrapper,
+} from "./EditModal.css";
 
 const EditModal = () => {
   const dispatch = useTypedDispatch();
@@ -91,33 +102,40 @@ const EditModal = () => {
   };
 
   return (
-    <div>
-      <div>
-        <div>
-          <div>{editingState.task.taskName}</div>
-          <FiX onClick={handleCloseButton} />
+    <div className={wrapper}>
+      <div className={modalWindow}>
+        <div className={header}>
+          <div className={title}>{editingState.task.taskName}</div>
+          <FiX className={closeButton} onClick={handleCloseButton} />
         </div>
-        <div>제목</div>
+        <div className={title}>제목</div>
         <input
+          className={input}
           type="text"
           value={data.task.taskName}
           onChange={handleNameChange}
         />
-        <div>설명</div>
+        <div className={title}>설명</div>
         <input
+          className={input}
           type="text"
           value={data.task.taskDescription}
           onChange={handleDescriptionChange}
         />
-        <div>생성한 사람</div>
+        <div className={title}>생성한 사람</div>
         <input
+          className={input}
           type="text"
           value={data.task.taskOwner}
           onChange={handleAuthorChange}
         />
-        <div>
-          <button onClick={handleUpdate}>일 수정하기</button>
-          <button onClick={handleDelete}>일 삭제하기</button>
+        <div className={buttons}>
+          <button onClick={handleUpdate} className={updateButton}>
+            일 수정하기
+          </button>
+          <button onClick={handleDelete} className={deleteButton}>
+            일 삭제하기
+          </button>
         </div>
       </div>
     </div>
