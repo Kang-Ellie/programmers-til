b6f44911859af4548e5b5ebb01e2b0cd94a9c220
Ellie-kang <kya6873@hanmail.net>
2026-03-25 20:30:04 +0900

    ✨ feat: Firebase 프로젝트 연결 및 초기 설정
    - firebase.ts 설정 파일 생성 및 SDK 초기화

---
+++ task-app/src/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBCfhXLeytlZxGXfvnrItCibUaCmxSdYig",
+  authDomain: "task-app-71fd5.firebaseapp.com",
+  projectId: "task-app-71fd5",
+  storageBucket: "task-app-71fd5.firebasestorage.app",
+  messagingSenderId: "139838912670",
+  appId: "1:139838912670:web:1ddcebc5ed4d1c8a6b238f",
+  measurementId: "G-Z1YLTZYRMS",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
